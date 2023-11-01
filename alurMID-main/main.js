eea17dbdbc36d7412edd6c71c7eb6f205c95bb14
viniciuslukasak <128619775+viniciuslukasak@users.noreply.github.com>
2023-11-01 08:18:36 -0300

    Add files via upload

---
+++ alurMID-main/main.js
@@ -0,0 +1,16 @@
+function tocaSomPom(idElementoAudio){
+    document.querySelector(idElementoAudio).
+}
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador < 9){
+
+ const instrumento = ListaDeTeclas[contador]
+
+ const tecla = ListaDeTeclas[contador];
+
+const idAudio = `#som_${instrumento}`;
+
